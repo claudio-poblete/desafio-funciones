931d2f213a38cca6fd2083807b1a72643c65086b
Claudio Poblete <pobleteclaudio2103@gmail.com>
2024-04-10 23:52:19 -0400

    modificación funciones y css

--- assets/js/teclas.js
+++ assets/js/teclas.js
@@ -1,21 +1,34 @@
-const box = document.querySelector('#box')
-const boxDos = document.querySelector('#box-dos')
+let box = document.querySelector('#box')
+let container = document.querySelector('#container')
+let div = document.createElement('div')
 
-document.addEventListener('keydown', function (event){
+document.addEventListener('keydown', (event)=>{
   if(event.key === 'a'){
     box.style.backgroundColor = 'pink'
   }else if(event.key === 's'){
     box.style.backgroundColor = 'orange'
   }else if(event.key === 'd'){
     box.style.backgroundColor = 'skyblue'
-  }else if(event.key === 'q'){
-    boxDos.style.backgroundColor = 'purple'
-  }else if(event.key === 'w'){
-    boxDos.style.backgroundColor = 'grey'
-  }else if(event.key === 'e'){
-    boxDos.style.backgroundColor = 'brown'
   }else{
-    box.style.backgroundColor = 'none'
-    boxDos.style.backgroundColor = 'none'
+    box.style.backgroundColor = 'black'
+    boxDos.style.backgroundColor = 'black'
+  }
+})
+
+document.addEventListener('keydown', (event)=>{
+  if(event.key === 'q'){
+    div.setAttribute('class', 'box')
+    div.style.backgroundColor = 'purple'
+    container.append(div)
+  }else if(event.key === 'w'){
+    div.setAttribute('class', 'box')
+    div.style.backgroundColor = 'grey'
+    container.append(div)
+  }else if(event.key === 'e'){
+    div.setAttribute('class', 'box')
+    div.style.backgroundColor = 'brown'
+    container.append(div)
+  }else{
+    container.removeChild(div)
   }
 })
